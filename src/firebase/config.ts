ea2ffa0cdb490bbb651c5009b000cfabfbef4a3c
ProxyHD <rodrigo09proxyhd@gmail.com>
2026-03-11 23:18:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5170428571-84962",
+  "appId": "1:285848134064:web:cb107e2ef3f311bdc77675",
+  "apiKey": "AIzaSyAy6gKkWlysMqZ5EIyhBau9jxoN853VmnQ",
+  "authDomain": "studio-5170428571-84962.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "285848134064"
+};
